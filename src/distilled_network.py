0edc1ecc0ca339c8d4be7a8a0ef5ac2271c2e4da
Amanda Olmin <amaol67@liu.se>
2019-06-26 15:10:27 +0200

    Extra file for distilled network

--- src/distilled_network.py
+++ src/distilled_network.py
@@ -17,7 +17,7 @@ class NeuralNet(nn.Module):
 
         self.layers = [self.fc1, self.fc2, self.fc3]
 
-    def forward(self, x, t=1):
+    def forward(self, x):
 
         assert x.size[1] == self.input_size
 
@@ -25,14 +25,10 @@ class NeuralNet(nn.Module):
             x = nn.functional.relu(layer(x))
 
             if i == (len(self.layers) - 1):
-                x = self.temperature_softmax(x, t)
+                x = self.temperature_softmax(x)
         return x
 
-    @staticmethod
-    def temperature_softmax(x, t=1):
-        return nn.functional.softmax(x / t)
-
-
+    
 def main():
     net = NeuralNet(20, 10, 5, 2)
     print(net)
