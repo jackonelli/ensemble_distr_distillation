ccada1bc1bce5455a76d608b62292caa6298f902
Amanda Olmin <amaol67@liu.se>
2019-06-26 15:47:09 +0200

    Merge fix

--- src/distilled_network.py
+++ src/distilled_network.py
@@ -17,7 +17,7 @@ class NeuralNet(nn.Module):
 
         self.layers = [self.fc1, self.fc2, self.fc3]
 
-    def forward(self, x):
+    def forward(self, x, t=1):
 
         assert x.size[1] == self.input_size
 
@@ -25,10 +25,10 @@ class NeuralNet(nn.Module):
             x = nn.functional.relu(layer(x))
 
             if i == (len(self.layers) - 1):
-                x = self.temperature_softmax(x)
+                x = nn.functional.softmax(x, t)
         return x
 
-    
+
 def main():
     net = NeuralNet(20, 10, 5, 2)
     print(net)
