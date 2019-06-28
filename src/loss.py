8c421c7a5ffacb2704b402eff52033e38ef787e9
Amanda Olmin <amaol67@liu.se>
2019-06-28 14:21:35 +0200

    Kan lossen fungera?

--- src/loss.py
+++ src/loss.py
@@ -13,6 +13,13 @@ class CrossEntropyLossOneHot(torch.autograd.Function):
     @staticmethod
     def backward(ctx, grad_output):
         inputs, soft_targets = ctx.saved_tensors
-        grad_input = grad_output * soft_targets
+        diag_mat = torch.eye(inputs.shape[1])
+
+        grad_input = list()
+        for i, inp in enumerate(inputs):
+            grad_input.append(grad_output[i, :] * soft_targets[i, :]
+                              * (diag_mat - nn.functional.softmax(inp)))
+
+        grad_input = torch.stack(grad_input)
 
         return grad_input
