9ab76b9a2c865da2562c82c5f48f10a969f09245
Jakob <jakob.lindqvist@chalmers.se>
2019-06-28 10:24:45 +0200

    entropy in metrics module

--- src/metrics.py
+++ src/metrics.py
@@ -2,8 +2,13 @@
 import torch
 
 
-def nll(true_labels, predicted_labels):
+def entropy(predicted_distribution):
+    """Labels as one hot vectors"""
+    return -torch.sum(
+        predicted_distribution * torch.log(predicted_distribution))
+
+
+def nll(true_labels, predicted_distribution):
     """Labels as one hot vectors"""
     true_labels_float = true_labels.float()
-    predicted_labels_float = predicted_labels.float()
-    return -torch.sum(true_labels_float * torch.log(predicted_labels_float))
+    return -torch.sum(true_labels_float * torch.log(predicted_distribution))
--- test/test_metrics.py
+++ test/test_metrics.py
@@ -5,13 +5,18 @@ from src import metrics
 
 
 class TestMetrics(unittest.TestCase):
+    def test_entropy(self):
+        predictions = torch.tensor([0.3, 0.7])
+        entropy = metrics.entropy(predictions)
+        self.assertAlmostEqual(entropy.item(), 0.61086430205)
+
     def test_nll(self):
         true_label = torch.tensor(1)
         num_classes = 2
         true_one_hot = utils.to_one_hot(true_label, num_classes)
         predictions = torch.tensor([0.5, 0.5])
         nll = metrics.nll(true_one_hot, predictions)
-        self.assertAlmostEquals(nll.item(), 0.69314718)
+        self.assertAlmostEqual(nll.item(), 0.69314718)
 
 
 if __name__ == '__main__':
