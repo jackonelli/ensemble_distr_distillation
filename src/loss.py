cbbc51bc66105ea7ff0f6200ce5a180b36f90a92
Jakob <jakob.lindqvist@chalmers.se>
2019-06-28 15:12:35 +0200

    This is my untested version of scalar soft loss

--- src/loss.py
+++ src/loss.py
@@ -1,25 +1,17 @@
+"""Loss module"""
 import torch
 import torch.nn as nn
 
 
 class CrossEntropyLossOneHot(torch.autograd.Function):
-
     @staticmethod
     def forward(ctx, inputs, soft_targets):
+        """TODO: extract convergence measure"""
         ctx.save_for_backward(inputs, soft_targets)
-        x = - soft_targets * nn.functional.log_softmax(inputs, dim=-1)
-        return x
+        return torch.sum(-soft_targets * nn.functional.log_softmax(inputs), -1)
 
     @staticmethod
     def backward(ctx, grad_output):
         inputs, soft_targets = ctx.saved_tensors
-        diag_mat = torch.eye(inputs.shape[1])
 
-        grad_input = list()
-        for i, inp in enumerate(inputs):
-            grad_input.append(grad_output[i, :] * soft_targets[i, :]
-                              * (diag_mat - nn.functional.softmax(inp)))
-
-        grad_input = torch.stack(grad_input)
-
-        return grad_input
+        return -soft_targets / inputs
