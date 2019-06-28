2aef71f36fac2f7eb29ec99c88141f79648adf73
Jakob <jakob.lindqvist@chalmers.se>
2019-06-28 14:41:27 +0200

    Docs

--- src/metrics.py
+++ src/metrics.py
@@ -4,24 +4,58 @@ import torch
 
 def entropy(predicted_distribution):
     """Entropy
-    Note: if a batch with N samples is given,
-    then the output is a tensor with N values
+
+    B = batch size, C = num classes
+    Labels as one hot vectors
+    Note: if a batch with B samples is given,
+    then the output is a tensor with B values
+
+    Args:
+        predicted_distribution: torch.tensor((B, C))
+
+    Returns:
+        entropy(ies): torch.tensor(B,)
     """
+
     return -torch.sum(
-        predicted_distribution * torch.log(predicted_distribution), -1)
+        predicted_distribution * torch.log(predicted_distribution), dim=-1)
 
 
 def nll(true_labels, predicted_distribution):
     """Negative log likelihood
+
+    B = batch size, C = num classes
     Labels as one hot vectors
-    Note: if a batch with N samples is given,
-    then the output is a tensor with N values
+    Note: if a batch with B samples is given,
+    then the output is a tensor with B values
+
+    Args:
+        true_labels: torch.tensor((B, C))
+        predicted_distribution: torch.tensor((B, C))
+
+    Returns:
+        nll(s): torch.tensor(B,)
     """
+
     true_labels_float = true_labels.float()
     return -torch.sum(true_labels_float * torch.log(predicted_distribution),
-                      -1)
+                      dim=-1)
 
 
 def brier_score(true_labels, predicted_distribution):
-    """Labels as one hot vectors"""
+    """Negative log likelihood
+
+    B = batch size, C = num classes
+    Labels as one hot vectors
+    Note: if a batch with B samples is given,
+    then the output is a tensor with B values
+
+    Args:
+        true_labels: torch.tensor((B, C))
+        predicted_distribution: torch.tensor((B, C))
+
+    Returns:
+        Brier score(s): torch.tensor(B,)
+    """
+
     true_labels_float = true_labels.float()
