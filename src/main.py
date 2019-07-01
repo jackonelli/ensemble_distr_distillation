1144ed244a43d800e1a65a8e07691b17abaa80e7
Jakob <jakob.lindqvist@chalmers.se>
2019-07-01 14:42:04 +0200

    Removed junk

--- src/main.py
+++ src/main.py
@@ -2,12 +2,9 @@
 from pathlib import Path
 import numpy as np
 import torch
-import torchvision.transforms as torch_transforms
 from dataloaders import gaussian
 import utils
 import models
-import extra_models
-import torchvision
 import distilled_network
 
 
@@ -40,23 +37,5 @@ def main():
     distilled_model.train(train_loader, args.num_epochs)
 
 
-def main_dump():
-    transform = torch_transforms.Compose([
-        torch_transforms.ToTensor(),
-        torch_transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
-    trainset = torchvision.datasets.CIFAR10(root='./data',
-                                            train=True,
-                                            download=True,
-                                            transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset,
-                                              batch_size=4,
-                                              shuffle=True,
-                                              num_workers=2)
-    net = extra_models.DumpNet()
-    net.train_full(trainloader)
-
-
 if __name__ == "__main__":
-    #main_dump()
     main()
