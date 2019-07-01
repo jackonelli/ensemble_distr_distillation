86add74e0dac6648dc2de2635ae17c1d4b7a4c02
Jakob <jakob.lindqvist@chalmers.se>
2019-07-01 15:00:32 +0200

    Both scalar and vector loss. Fixed deprecation

--- src/loss.py
+++ src/loss.py
@@ -1,8 +1,24 @@
+"""Loss module"""
 import torch
 import torch.nn as nn
 
 
 class CrossEntropyLossOneHot(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, inputs, soft_targets):
+        """TODO: extract convergence measure"""
+        ctx.save_for_backward(inputs, soft_targets)
+        return torch.sum(-soft_targets * nn.functional.log_softmax(inputs),
+                         dim=-1)
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        inputs, soft_targets = ctx.saved_tensors
+
+        return -soft_targets / inputs
+
+
+class CrossEntropyLossOneHotMatrix(torch.autograd.Function):
     @staticmethod
     def forward(ctx, inputs, soft_targets):
         ctx.save_for_backward(inputs, soft_targets)
