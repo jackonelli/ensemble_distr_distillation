daad037789fab60a2eb3ad158a4fb1b873f62224
Jakob <jakob.lindqvist@chalmers.se>
2019-09-05 11:26:56 +0200

    Fixed imports (At least for me)
    I don't think that these changes are breaking, but please confirm.
    In short:
    
     - __init__.py files are apparently not needed for python > 3.3
     - To run scripts in subdirs, e.g.
       src/experiments/synthetic_data_classification.py is not possible
       without adding src to your path.

---
+++ src/ensemble/simple_classifier.py
@@ -0,0 +1,50 @@
+import torch
+import torch.optim as torch_optim
+import torch.nn as nn
+from ensemble import ensemble
+
+
+class SimpleClassifier(ensemble.EnsembleMember):
+    def __init__(self,
+                 input_size,
+                 hidden_size_1,
+                 hidden_size_2,
+                 output_size,
+                 device=torch.device("cpu"),
+                 learning_rate=0.001):
+        super().__init__(loss_function=nn.CrossEntropyLoss(), device=device)
+        self.input_size = input_size
+        self.hidden_size_1 = hidden_size_1  # Or make a list or something
+        self.hidden_size_2 = hidden_size_2
+        self.output_size = output_size
+        self.learning_rate = learning_rate
+
+        self.fc1 = nn.Linear(self.input_size, self.hidden_size_1)
+        self.fc2 = nn.Linear(self.hidden_size_1, self.hidden_size_2)
+        self.fc3 = nn.Linear(self.hidden_size_2, self.output_size)
+
+        self.layers = [self.fc1, self.fc2, self.fc3]
+        self.optimizer = torch_optim.SGD(self.parameters(),
+                                         lr=self.learning_rate,
+                                         momentum=0.9)
+        self.to(self.device)
+
+    def forward(self, x):
+        x = nn.functional.relu(self.fc1(x))
+        x = nn.functional.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+    @staticmethod
+    def temperature_softmax(x, t=1):
+        return nn.functional.softmax(x / t, dim=-1)
+
+    def calculate_loss(self, outputs, labels):
+
+        return self.loss(outputs, labels.type(torch.LongTensor))
+
+    def predict(self, x, t=1):
+        x = self.forward(x)
+        x = self.temperature_softmax(x, t)
+
+        return x
