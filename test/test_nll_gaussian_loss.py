bd73c428e233907d51c2129952f809442f1abfbe
Jakob <jakob.lindqvist@chalmers.se>
2020-03-26 16:14:05 +0100

    1D suff stats gauss nll

--- test/test_nll_gaussian_loss.py
+++ test/test_nll_gaussian_loss.py
@@ -2,23 +2,28 @@ import torch
 import unittest
 import math
 import src.loss as loss
+import math
 
 NUM_DECIMALS = 5
 
 
-class TestGaussianLoss(unittest.TestCase):
-    def test_gaussian_nll_one_dim(self):
-        B, N, D = 1, 1, 1
-        target = torch.tensor([[1.0]], dtype=torch.float).reshape(B, N, D)
-        mean = torch.tensor([[0.5]], dtype=torch.float)
-        var = torch.tensor([[10]], dtype=torch.float)
+class TestGaussianNll(unittest.TestCase):
+    def test_one_dim(self):
+        B, N, D = 1, 2, 1
+        target = torch.tensor([[1.0, -2.0]],
+                              dtype=torch.float).reshape(B, N, D)
+        mean = torch.tensor([[0.0]], dtype=torch.float)
+        var = torch.tensor([[1.0]], dtype=torch.float)
 
-        gauss_nll = loss.gaussian_neg_log_likelihood((mean, var), target)
-        self.assertAlmostEqual(gauss_nll.item(),
-                               0.5 * math.log(2 * math.pi * 10) + 0.0125,
-                               places=NUM_DECIMALS)
+        true_sample_avg = -1 / 2
+        true_sample_cov = 9 / 4
 
-    def test_gaussian_nll_two_dim(self):
+        gauss_nll = loss.gaussian_nll_1d((mean, var), target)
+        true_nll = (math.log(2 * math.pi) + math.log(1) +
+                    ((true_sample_avg - 0)**2 + true_sample_cov) / 1) / 2
+        self.assertAlmostEqual(gauss_nll.item(), true_nll, places=NUM_DECIMALS)
+
+    def test_two_dim(self):
         B, N, D = 1, 1, 2
         target = torch.tensor([[1.0, 2.0]], dtype=torch.float).reshape(B, N, D)
         mean = torch.tensor([[0.5, 1.0]], dtype=torch.float)
@@ -30,7 +35,7 @@ class TestGaussianLoss(unittest.TestCase):
                                0.1625,
                                places=NUM_DECIMALS)
 
-    def test_gaussian_nll_two_ensemble_members(self):
+    def test_two_ensemble_members(self):
         B, N, D = 1, 2, 1
         target = torch.unsqueeze(torch.tensor([[1.0, 0.75]],
                                               dtype=torch.float),
@@ -38,35 +43,26 @@ class TestGaussianLoss(unittest.TestCase):
         mean = torch.tensor([[0.5]], dtype=torch.float)
         var = torch.unsqueeze(torch.tensor([10.0], dtype=torch.float), dim=-1)
 
-        gauss_nll = loss.gaussian_neg_log_likelihood((mean, var), target)
+        gauss_nll = loss.gaussian_nll_1d((mean, var), target)
         self.assertAlmostEqual(gauss_nll.item(),
                                0.25 * math.log(4 * math.pi**2 * 10**2) +
                                (0.015625 / 2),
                                places=NUM_DECIMALS)
 
-    def test_gaussian_nll_batch(self):
+    def test_batch(self):
         B, N, D = 2, 1, 1
         target = torch.tensor([[1.0], [0.75]],
                               dtype=torch.float).reshape(B, N, D)
         mean = torch.tensor([[0.5], [0.25]], dtype=torch.float)
         var = torch.tensor([[10.0], [5.0]], dtype=torch.float)
 
-        gauss_nll = loss.gaussian_neg_log_likelihood((mean, var), target)
-        self.assertAlmostEqual(gauss_nll.item(),
-                               0.25 * math.log(4 * math.pi**2 * 10 * 5) +
-                               0.01875,
+        gauss_nll = loss.gaussian_nll_1d((mean, var), target)
+        true_nll_1 = (math.log(2 * math.pi) + math.log(10.0) +
+                      ((1.0 - 0.5)**2 + 0) / 10.0) / 2
+        true_nll_2 = (math.log(2 * math.pi) + math.log(5.0) +
+                      ((0.75 - 0.25)**2 + 0) / 5.0) / 2
+        self.assertAlmostEqual(gauss_nll.item(), (true_nll_1 + true_nll_2) / 2,
                                places=NUM_DECIMALS)
-
-    # def test_gaussian_nll_unopt(self):
-    #     target = torch.tensor([[[1.0], [0.75]]], dtype=torch.float)
-    #     mean = torch.tensor([[0.5], [0.25]], dtype=torch.float)
-    #     var = torch.tensor([[10.0], [5.0]], dtype=torch.float)
-
-    #     gauss_nll = loss.gaussian_neg_log_likelihood_unopt((mean, var), target)
-    #     self.assertAlmostEqual(gauss_nll.item(),
-    #                            0.25 * math.log(4 * math.pi**2 * 10 * 5) +
-    #                            0.01875,
-    #                            places=5)
 
 
 if __name__ == '__main__':
